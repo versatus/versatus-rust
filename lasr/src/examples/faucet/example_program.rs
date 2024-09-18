3ee822e6009b6e4ad5107884044981503193ac79
Andrew O'Brien <ohbandrew@gmail.com>
2024-09-18 00:48:39 -0500

    move comments in example programs/convert to module level doc comment

--- lasr/src/examples/faucet/example_program.rs
+++ lasr/src/examples/faucet/example_program.rs
@@ -1,10 +1,10 @@
+//! A minimalistic main function for a Rust LASR program.
+//! A Program that takes in lasr_type::Inputs, handles the call based on the program method, and produces necessary lasr_types::Outputs to be processed by protocol
 use crate::lasrctl::builders::program::Program;
 use lasr_types::Inputs;
 use std::fs;
 
 #[allow(dead_code)]
-/// A minimalistic main function for a Rust LASR program.
-/// A Program that takes in lasr_type::Inputs, handles the call based on the program method, and produces necessary lasr_types::Outputs to be processed by protocol
 fn main() -> anyhow::Result<()> {
     if let Err(e) = Program::run() {
         bail!("Error occured while running program: {}", e.to_string())
